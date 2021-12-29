ff7c41de43ae4841a498bd0ca5965a8c80a5b166
Anupam G <anupam@wso2.com>
2021-12-29 11:47:56 -0300

    demo codes added

---
+++ go-docker/cmd/app.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+func ProcessLine(ch chan string) {
+	i := 0
+	for {
+		i = i + 1
+		fmt.Println("Receiving line", i)
+		line := <-ch
+		fmt.Println(line)
+	}
+
+}
+func ReadFile() {
+	ch := make(chan string)
+	go ProcessLine(ch)
+
+	file, err := os.Open("/Users/agogoi/git/anupamgogoi0907/go-apps/go-docker/go.sum")
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		time.Sleep(time.Second * 5)
+		ch <- scanner.Text()
+	}
+}

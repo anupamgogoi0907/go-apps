d9c71ec5f3873ad76fa367c89526e9385de70d76
Anupam G <anupam@wso2.com>
2021-12-31 17:58:48 -0300

    saaas

--- go-docker/test/buffered_channel_test.go
+++ go-docker/test/buffered_channel_test.go
@@ -6,22 +6,8 @@ import (
 	"time"
 )
 
-// Worker
-func Worker(id int, jobs chan int, result chan int) {
-	for j := range jobs {
-		time.Sleep(time.Second * 2)
-		fmt.Println("Worker: ", id, ", Jobs done: ", j)
-		result <- j * 1000
-	}
-}
-
-func PrintResult(results chan int) {
-	for {
-		fmt.Println(<-results)
-	}
-}
-
-func TestBufferedChannel(t *testing.T) {
+// TestMultipleWorker
+func TestMultipleWorker(t *testing.T) {
 	const numJobs = 3
 	jobs := make(chan int, numJobs)
 	results := make(chan int, numJobs)
@@ -38,4 +24,36 @@ func TestBufferedChannel(t *testing.T) {
 	close(jobs)
 
 	time.Sleep(time.Minute)
+}
+func Worker(id int, jobs chan int, result chan int) {
+	for j := range jobs {
+		time.Sleep(time.Second * 2)
+		fmt.Println("Worker: ", id, ", Jobs done: ", j)
+		result <- j * 1000
+	}
+}
+
+func PrintResult(results chan int) {
+	for {
+		fmt.Println(<-results)
+	}
+}
+
+// TestSingleWorker
+func TestSingleWorker(t *testing.T) {
+	jobs := make(chan int, 2)
+
+	go TempWorker(1, jobs)
+
+	jobs <- 10
+	jobs <- 20
+	jobs <- 20
+
+	time.Sleep(time.Minute)
+}
+
+func TempWorker(id int, jobs chan int) {
+	for {
+		fmt.Println("Worker: ", id, " ,Jobs: ", <-jobs)
+	}
 }

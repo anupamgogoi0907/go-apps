9ff8f357665db274c2c1d1f153ac0152413d500d
Anupam G <anupam@wso2.com>
2022-01-07 17:29:58 -0300

    more code added

--- go-docker/pkg/pool/file_processor_pool.go
+++ go-docker/pkg/pool/file_processor_pool.go
@@ -1,14 +1,12 @@
 package pool
 
 import (
-	"bufio"
-	"log"
-	"os"
+	"fmt"
 	"sync"
 )
 
 var chLines = make(chan []string, 5)
-var chResults = make(chan []string, 5)
+var chResults = make(chan []string)
 
 type FileWorkerPool struct {
 	FilePath           string
@@ -19,65 +17,38 @@ type FileWorkerPool struct {
 
 func (fw *FileWorkerPool) Run() {
 	fw.waitGroup = &sync.WaitGroup{}
-	go fw.ReadFileContent()
-
-	// Allocate NoOfWorkers workers to process file lines.
-	for i := 1; i <= fw.NoOfWorkers; i++ {
-		fw.waitGroup.Add(1)
-		go fw.produceResultsWorker(i)
-	}
+	go fw.consumeResults()
+	fw.createWorkerPool()
+	fw.ReadFileContent()
 	fw.waitGroup.Wait()
-
-	log.Println("Executed Workers########")
-	close(chResults)
+}
+func (fw *FileWorkerPool) createWorkerPool() {
+	for i := 1; i <= fw.NoOfWorkers; i++ {
+		go fw.produceResults(i)
+	}
+}
+func (fw *FileWorkerPool) ReadFileContent() {
+	for i := 1; i <= 10; i++ {
+		chLines <- []string{"Hello"}
+	}
+	close(chLines)
 }
 
-// 10 produceResultsWorker are working concurrently to process data sent to chLines channel.
-func (fw *FileWorkerPool) produceResultsWorker(workerId int) {
-	for l := range chLines {
-		// Add processing code.
-		log.Println("Worker:", workerId, "is processing.", l)
-		//chResults <- []string{"Processed"}
-		log.Println("Worker:", workerId, "has processed.")
+func (fw *FileWorkerPool) produceResults(id int) {
+	fw.waitGroup.Add(1)
+	for j := range chLines {
+		fmt.Println("Worker:", id, "is processing:", j)
+		chResults <- j
+		fmt.Println("Worker:", id, "has processed:", j)
 	}
 	fw.waitGroup.Done()
 }
-func (fw *FileWorkerPool) consumeResultsWorker() {
-	for r := range chResults {
-		log.Println(r)
-	}
-}
 
-func (fw *FileWorkerPool) ReadFileContent() {
+func (fw *FileWorkerPool) consumeResults() {
 	fw.waitGroup.Add(1)
-
-	if fw.FilePath == "" {
-		log.Panicln("No file provided.")
+	for i := 1; i <= 10; i++ {
+		fmt.Println(<-chResults)
+		fmt.Println("Results consumed.")
 	}
-	file, err := os.Open(fw.FilePath)
-	if err != nil {
-		log.Panicln(err)
-	}
-	scanner := bufio.NewScanner(file)
-
-	// Count the number of batches of lines sent to chLines
-	lineBuffer := []string{}
-	for scanner.Scan() {
-		if len(lineBuffer) >= fw.NoOfLinesToProcess {
-			// Send noOfLines to chLines channel to process. After that clean the lineBuffer.
-			log.Println("Send lineBuffer to chLines to be processed.")
-			chLines <- lineBuffer
-			lineBuffer = []string{}
-		}
-		line := scanner.Text()
-		lineBuffer = append(lineBuffer, line)
-	}
-	// Check if there is remaining lines in the lineBuffer.
-	if len(lineBuffer) != 0 {
-		chLines <- lineBuffer
-		lineBuffer = nil
-	}
-	close(chLines)
-	defer file.Close()
 	fw.waitGroup.Done()
 }

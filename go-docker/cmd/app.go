1ffc98bdedc3001e74b04baa4ec653f94a3a0459
Anupam G <anupam@wso2.com>
2022-01-15 12:02:16 -0300

    enter

--- go-docker/cmd/app.go
+++ go-docker/cmd/app.go
@@ -8,34 +8,44 @@ import (
 
 func main() {
 	wg := sync.WaitGroup{}
-	result1 := stage1([]int{1, 2, 3}, &wg)
-
-	// stage2 is polling on result1
-	result2 := stage2(result1, &wg)
-
-	// stage3 is polling on result2
-	result3 := stage3(result2, &wg)
-
-	// finish is polling on result3
-	finish(result3)
+	pipeline([]int{1, 2}, &wg)
 	wg.Wait()
 }
 
-func stage1(nums []int, wg *sync.WaitGroup) chan string {
+func pipeline(data []int, wg *sync.WaitGroup) {
 	wg.Add(1)
-	result := make(chan string)
+	in := make(chan string)
+	go func() {
+		for _, n := range data {
+			v := strconv.Itoa(n)
+			in <- v
+		}
+		close(in)
+		wg.Done()
+	}()
 
-	worker := func(workerId int) {
-		for _, n := range nums {
-			fmt.Printf("RECEIVING:Stage1,Worker:%d,Data:%d,Source Channel:%x,Target Channel:%x\n", workerId, n, nil, result)
-			v := strconv.Itoa(n) + "->stage1"
+	result1 := stage1(in, wg)
+	result2 := stage2(result1, wg)
+	result3 := stage3(result2, wg)
+	finish(result3)
+}
+
+func stage1(in chan string, wg *sync.WaitGroup) chan string {
+	result := make(chan string)
+	wg.Add(1)
+
+	worker := func(workerId int, in chan string) {
+		for n := range in {
+			fmt.Printf("RECEIVING.Stage1,Worker:%d,Data:%s,Source Channel:%x,Target Channel:%x\n", workerId, n, in, result)
+			v := n + "->stage1"
 			result <- v
 			fmt.Printf("SENT:Stage1,Worker:%d,Data:%s,Target Channel:%x\n", workerId, v, result)
 		}
 		close(result)
 		wg.Done()
 	}
-	go worker(1)
+	go worker(1, in)
+
 	return result
 }
 

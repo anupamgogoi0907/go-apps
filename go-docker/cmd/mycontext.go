f695b556da93b1751c654b6be33585cd60df9662
Anupam G <anupam@wso2.com>
2022-02-02 17:49:10 -0300

    dss

--- go-docker/cmd/mycontext.go
+++ go-docker/cmd/mycontext.go
@@ -11,36 +11,46 @@ var result = make(chan string)
 func main() {
 	wg := sync.WaitGroup{}
 
-	out := processor1(&wg)
-	processor2(out, &wg)
+	out, done := processor1(&wg)
+	processor2(out, done, &wg)
 	wg.Wait()
 }
 
-func processor1(wg *sync.WaitGroup) chan int {
+func processor1(wg *sync.WaitGroup) (chan int, chan bool) {
+	const NumSenders = 2
 	ch := make(chan int)
-
+	done := make(chan bool, NumSenders)
 	// Worker
-	worker := func(id int, out chan int, wg *sync.WaitGroup) {
-		defer wg.Done()
-		v := rand.Intn(5)
-		for i := 0; i < v; i++ {
-			fmt.Printf("SENDING:Channel::%x, Worker:%d, Value:%d\n", out, i, v)
+	worker := func(workerId int, out chan int, wg *sync.WaitGroup) {
+		n := rand.Intn(5)
+		fmt.Printf("### Worker:%d, Total Values:%d\n", workerId, n)
+		for i := 0; i < n; i++ {
+			fmt.Printf("Value:%d\n", i)
 			out <- i
 		}
+		// Signal something that it has done processing.
+		//done <- true
+		wg.Done()
 	}
 
 	// No of workers
-	const NumSenders = 2
 	for i := 1; i <= NumSenders; i++ {
 		wg.Add(1)
 		go worker(i, ch, wg)
 	}
-	return ch
+	return ch, done
 }
 
-func processor2(ch chan int, wg *sync.WaitGroup) {
-	select {
-	case data := <-ch:
-		fmt.Println("RECEIVING::", data)
+func processor2(ch chan int, done chan bool, wg *sync.WaitGroup) {
+	consumer := func() {
+		for {
+			select {
+			case d := <-ch:
+				fmt.Println("RECEIVED:", d)
+			}
+			//wg.Done()
+		}
 	}
+	//wg.Add(1)
+	go consumer()
 }

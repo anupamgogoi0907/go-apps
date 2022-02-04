a050c179caffd92b7374b92564895c05305de175
Anupam G <anupam@wso2.com>
2022-02-04 17:29:28 -0300

    Revert "Test case modified"
    This reverts commit fd8c4417

--- go-docker/test/atomic_test.go
+++ go-docker/test/atomic_test.go
@@ -11,45 +11,32 @@ func TestAtomic(t *testing.T) {
 	wg := sync.WaitGroup{}
 
 	var n uint64
-	ch := p1(n, &wg)
-	p2(ch, n, &wg)
+	ch := p1(&n, &wg)
+	p2(ch, &n, &wg)
 	wg.Wait()
 }
 
-func p1(n uint64, wg *sync.WaitGroup) chan int {
+func p1(n *uint64, wg *sync.WaitGroup) chan int {
 	ch := make(chan int)
-	worker := func(workerId int, wg *sync.WaitGroup) {
-		for i := 10; i <= 15; i++ {
-			ch <- i
-			fmt.Printf("### Worker:%d, Data sent:%d\n", workerId, i)
-		}
+	worker := func(wg *sync.WaitGroup) {
+		ch <- 10
 		wg.Done()
-		atomic.AddUint64(&n, 1)
-		fmt.Println("Done:", workerId)
+		atomic.AddUint64(n, 10000)
+		fmt.Println("Done")
 	}
 
-	const NumOfWorkers = 2
-	for i := 1; i <= NumOfWorkers; i++ {
-		wg.Add(1)
-		go worker(i, wg)
-	}
+	wg.Add(1)
+	go worker(wg)
 	return ch
 }
 
-func p2(ch chan int, n uint64, wg *sync.WaitGroup) {
+func p2(ch chan int, n *uint64, wg *sync.WaitGroup) {
 	worker := func(wg *sync.WaitGroup) {
-		for {
-			c := atomic.LoadUint64(&n)
-			fmt.Println("Count:", c)
-			if c == 2 {
-				return
-			}
-			select {
-			case data := <-ch:
-				fmt.Println(data)
-			}
-		}
+		<-ch
+		fmt.Println(atomic.LoadUint64(n))
+		wg.Done()
 	}
 
+	wg.Add(1)
 	go worker(wg)
 }

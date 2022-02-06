4aabd2dec346bc56484c3aefd828aecadc1a145d
Anupam G <anupam@wso2.com>
2022-02-06 09:39:20 -0300

    simple context added

--- go-docker/cmd/simple-context.go
+++ go-docker/cmd/simple-context.go
@@ -1,5 +1,61 @@
 package main
 
-func InitSimpleContext() {
+import (
+	"context"
+	"fmt"
+	"sync"
+)
 
+func main() {
+	InitSimpleContext()
+}
+func InitSimpleContext() {
+	wg := sync.WaitGroup{}
+	ctxRoot := context.Background()
+	ctxCancel, cancel := context.WithCancel(ctxRoot)
+
+	data := make(chan int)
+	b1(data, cancel, ctxCancel, &wg)
+	b2(data, cancel, ctxCancel, &wg)
+	wg.Wait()
+}
+
+func b1(data chan int, cancel context.CancelFunc, ctxCancel context.Context, wg *sync.WaitGroup) {
+	worker := func(data chan int, ctxCancel context.Context, wg *sync.WaitGroup) {
+		defer wg.Done()
+		flag := true
+		d := 0
+		for flag {
+			if d == 5 {
+				flag = false
+				cancel()
+			} else {
+				d = d + 1
+				data <- d
+			}
+		}
+	}
+
+	wg.Add(1)
+	go worker(data, ctxCancel, wg)
+}
+func b2(data chan int, cancel context.CancelFunc, ctxCancel context.Context, wg *sync.WaitGroup) {
+	worker := func(data chan int, ctxCancel context.Context, wg *sync.WaitGroup) {
+		defer wg.Done()
+		flag := true
+		for flag {
+			select {
+			case <-ctxCancel.Done():
+				fmt.Println("B2 exited.")
+				flag = false
+			case d := <-data:
+				fmt.Printf("<<< B2, Received:%d\n", d)
+			default:
+
+			}
+		}
+	}
+
+	wg.Add(1)
+	go worker(data, ctxCancel, wg)
 }

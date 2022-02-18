6199fb3104cf22a020a0d913c04a6d24a541011f
Anupam G <anupam@wso2.com>
2022-02-18 15:22:40 -0300

    Struct added

--- data-processor/pkg/pipeline/ingest.go
+++ data-processor/pkg/pipeline/ingest.go
@@ -8,24 +8,43 @@ import (
 	"sync"
 )
 
-func ReadLargeFile(path string) error {
+type DataReader struct {
+	Path      string
+	ChunkPool *sync.Pool
+	LinePool  *sync.Pool
+}
+
+func InitDataReader(path string) {
+	chunkPool := sync.Pool{New: func() interface{} {
+		buffer := make([]byte, 50*1024)
+		return buffer
+	}}
+	linePool := sync.Pool{New: func() interface{} {
+		lines := ""
+		return lines
+	}}
+
+	dataReader := &DataReader{
+		Path:      path,
+		ChunkPool: &chunkPool,
+		LinePool:  &linePool,
+	}
+	ReadLargeFile(dataReader)
+}
+func ReadLargeFile(dataReader *DataReader) error {
 	// Check for entered file path.
-	if path == "" {
+	if dataReader.Path == "" {
 		return errors.New("no path found")
 	}
 
 	// Check if the file can be opened.
-	file, err := os.Open(path)
+	file, err := os.Open(dataReader.Path)
 	if err != nil {
 		return err
 	}
 
 	// Pools for line
 	reader := bufio.NewReader(file)
-	linesPool := sync.Pool{New: func() interface{} {
-		lines := make([]byte, 50*1024)
-		return lines
-	}}
 
 	// Keep a count of number of chunks.
 	nChunks := 0
@@ -34,23 +53,26 @@ func ReadLargeFile(path string) error {
 	flag := true
 
 	for flag {
-		buffer := linesPool.Get().([]byte)
-		nBytes, err := reader.Read(buffer)
+		chunk := dataReader.ChunkPool.Get().([]byte)
+		nBytes, err := reader.Read(chunk)
 		if err != nil {
 			fmt.Println(err)
 			flag = false
 			break
 		}
 		nChunks = nChunks + 1
-		ProcessLine(&linesPool, buffer, nBytes)
+		ProcessLine(dataReader, chunk, nBytes)
 	}
 
 	fmt.Println("Total chunks:", nChunks)
 	return nil
 }
 
-func ProcessLine(linesPool *sync.Pool, buffer []byte, nBytes int) {
-	linesPool.Put(buffer)
-	line := buffer[0:nBytes]
-	fmt.Println(string(line))
+func ProcessLine(dataReader *DataReader, chunk []byte, nBytes int) {
+	line := dataReader.LinePool.Get().(string)
+	line = string(chunk[0:nBytes])
+	fmt.Println(line)
+
+	dataReader.ChunkPool.Put(chunk)
+	dataReader.LinePool.Put(line)
 }
--- data-processor/test/ingest_test.go
+++ data-processor/test/ingest_test.go
@@ -2,13 +2,9 @@ package test
 
 import (
 	"github.com/anupamgogoi0907/go-apps/data-processor/pkg/pipeline"
-	"log"
 	"testing"
 )
 
 func TestReadLargeFile(t *testing.T) {
-	err := pipeline.ReadLargeFile("/Users/agogoi/softwares/wso2/AM/wso2am-3.1.0/repository/logs/wso2carbon.log")
-	if err != nil {
-		log.Println(err)
-	}
+	pipeline.InitDataReader("/Users/agogoi/softwares/wso2/AM/wso2am-3.1.0/repository/logs/wso2carbon.log")
 }

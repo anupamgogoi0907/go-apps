2a2ce830a701bf8cf6f83e10308da769fca71d1c
Anupam G <anupam@wso2.com>
2022-02-18 15:44:54 -0300

    Data pipeline created

--- data-processor/pkg/pipeline/ingest.go
+++ data-processor/pkg/pipeline/ingest.go
@@ -12,11 +12,12 @@ type DataReader struct {
 	Path      string
 	ChunkPool *sync.Pool
 	LinePool  *sync.Pool
+	wg        *sync.WaitGroup
 }
 
 func InitDataReader(path string) {
 	chunkPool := sync.Pool{New: func() interface{} {
-		buffer := make([]byte, 50*1024)
+		buffer := make([]byte, 10*1024)
 		return buffer
 	}}
 	linePool := sync.Pool{New: func() interface{} {
@@ -28,6 +29,7 @@ func InitDataReader(path string) {
 		Path:      path,
 		ChunkPool: &chunkPool,
 		LinePool:  &linePool,
+		wg:        &sync.WaitGroup{},
 	}
 	ReadLargeFile(dataReader)
 }
@@ -61,18 +63,23 @@ func ReadLargeFile(dataReader *DataReader) error {
 			break
 		}
 		nChunks = nChunks + 1
-		ProcessLine(dataReader, chunk, nBytes)
+
+		dataReader.wg.Add(1)
+		go ProcessLine(dataReader, chunk, nBytes, nChunks)
 	}
+	dataReader.wg.Wait()
 
 	fmt.Println("Total chunks:", nChunks)
 	return nil
 }
 
-func ProcessLine(dataReader *DataReader, chunk []byte, nBytes int) {
+func ProcessLine(dataReader *DataReader, chunk []byte, nBytes int, nChunks int) {
 	line := dataReader.LinePool.Get().(string)
 	line = string(chunk[0:nBytes])
-	fmt.Println(line)
+	fmt.Printf("########## Chunk: %d ########## \n%s\n", nChunks, line)
 
+	// Put back chunk and line to the pools
 	dataReader.ChunkPool.Put(chunk)
 	dataReader.LinePool.Put(line)
+	dataReader.wg.Done()
 }

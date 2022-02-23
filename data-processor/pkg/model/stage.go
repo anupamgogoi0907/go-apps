61da1d36e0b0e3b3fa8b0a4479f5d7125fceb4ac
Anupam G <anupam@wso2.com>
2022-02-23 10:55:36 -0300

    scanner condition added

--- data-processor/pkg/model/stage.go
+++ data-processor/pkg/model/stage.go
@@ -1,10 +1,34 @@
 package model
 
+// Stage data structure
 type ProcessData func(stage *Stage)
-
 type Stage struct {
 	Id      int
 	Name    string
 	Process ProcessData
 	Next    *Stage
+}
+
+func NewStage(Id int, Name string, Process func(stage *Stage), Next *Stage) *Stage {
+	stage := Stage{
+		Id:      Id,
+		Name:    Name,
+		Process: Process,
+		Next:    Next,
+	}
+	return &stage
+}
+
+// StageData
+type StageData struct {
+	FilePath interface{}
+	KeyWord  interface{}
+}
+
+func NewStageData(FilePath interface{}, KeyWord interface{}) *StageData {
+	stageData := StageData{
+		FilePath: FilePath,
+		KeyWord:  KeyWord,
+	}
+	return &stageData
 }
--- data-processor/test/stage_test.go
+++ data-processor/test/stage_test.go
@@ -3,29 +3,19 @@ package test
 import (
 	"fmt"
 	"github.com/anupamgogoi0907/go-apps/data-processor/pkg/model"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestStage(t *testing.T) {
-	stage2 := model.Stage{
-		Id:   2,
-		Name: "Read File",
-		Process: func(stage *model.Stage) {
-			fmt.Println("My stage1 id is:", stage.Id)
-		},
-		Next: nil,
+	p1 := func(st *model.Stage) {
+		fmt.Println("Stage id:", st.Id)
 	}
-	stage1 := model.Stage{
-		Id:   1,
-		Name: "Read File",
-		Process: func(stage *model.Stage) {
-			fmt.Println("My stage1 id is:", stage.Id)
-		},
-		Next: &stage2,
-	}
-
-	fmt.Printf("Cur:%v, Next:%p\n", stage1, &stage2)
-
-	stage1.Next.Id = 1000
-	stage2.Process(&stage2)
+	stage1 := model.NewStage(1, "Stage 1", p1, nil)
+	stage1.Process(stage1)
+	assert.Equal(t, 1, stage1.Id)
+}
+func TestStageData(t *testing.T) {
+	stageData := model.NewStageData("demopath", "Hello World")
+	assert.Equal(t, "demopath", stageData.FilePath)
 }

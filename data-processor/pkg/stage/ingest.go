88c9bff32290549892a78a49d7460f5b718d9fbc
Anupam G <anupam@wso2.com>
2022-02-24 18:03:49 -0300

    workers modified

--- data-processor/pkg/stage/ingest.go
+++ data-processor/pkg/stage/ingest.go
@@ -8,8 +8,9 @@ import (
 )
 
 var (
-	chunkSize = 2
+	chunkSize = 10 * 1024
 	chunk     = make([]byte, chunkSize)
+	workers   = 3
 )
 
 type Ingest struct {
@@ -42,51 +43,43 @@ func NewIngest(path string, data chan string) *Ingest {
 	return dataReader
 }
 func (in *Ingest) ReadFileConcurrently() error {
-	// Check if the file can be opened.
-	file, err := os.Open(in.Path)
-	defer file.Close()
-	if err != nil {
-		return err
-	}
-
-	// Check file size.
-	fi, _ := file.Stat()
-	fileSize := int(fi.Size())
-
-	// Count number of necessary workers.
-	count := fileSize / chunkSize
-	if r := fileSize % chunkSize; r != 0 {
-		count++
-	}
-	fmt.Println("Total workers:", count)
-	// Reader.
 	offset := int64(0)
-	reader := bufio.NewReader(file)
 
 	// Spawn count number of goroutines.
-	for i := 1; i <= count; i++ {
+	for i := 1; i <= workers; i++ {
 		in.WG.Add(1)
-		go in.ReadFileConcurrentlyRoutine(i, offset, file, reader)
+		go in.ReadFileConcurrentlyRoutine(i, offset)
 		offset = offset + int64(chunkSize)
 	}
 	in.WG.Wait()
 	return nil
 }
-func (in *Ingest) ReadFileConcurrentlyRoutine(workerId int, offset int64, file *os.File, reader *bufio.Reader) error {
+func (in *Ingest) ReadFileConcurrentlyRoutine(workerId int, offset int64) error {
+	file, _ := os.Open(in.Path)
+	defer file.Close()
 	file.Seek(offset, 0)
+
+	// Get chunk and text from pools
 	chunk := in.ChunkPool.Get().([]byte)
+	text := in.TextPool.Get().(string)
+
+	// Get the reader and read file.
+	reader := bufio.NewReader(file)
 	nBytes, err := reader.Read(chunk)
 
 	if err != nil {
 		fmt.Printf("########## Worker:%d, Offset:%d ##########\n%s\n", workerId, offset, string(err.Error()))
 	} else {
-		text := in.TextPool.Get().(string)
+
 		text = string(chunk[0:nBytes])
 		fmt.Printf("########## Worker:%d, Offset:%d ##########\n%s\n", workerId, offset, text)
 		in.TextPool.Put(text)
 	}
 
+	// Put chunk and text back to the respective pools.
 	in.ChunkPool.Put(chunk)
+	in.TextPool.Put(text)
+
 	in.WG.Done()
 	return nil
 }

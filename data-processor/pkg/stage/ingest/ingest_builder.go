908db3757e3ae8e4bdf30dd70a662a32a25dc5ac
Anupam G <anupam@wso2.com>
2022-02-26 19:48:24 -0300

    Builde code separated

--- data-processor/pkg/stage/ingest/ingest_builder.go
+++ data-processor/pkg/stage/ingest/ingest_builder.go
@@ -1,1 +1,38 @@
 package ingest
+
+import "sync"
+
+type IngestBuilder interface {
+	Path(Path string) IngestBuilder
+	Build() *Ingest
+}
+
+type ingestBuilder struct {
+	path string
+}
+
+func (ib *ingestBuilder) Path(Path string) IngestBuilder {
+	ib.path = Path
+	return ib
+}
+func (ib *ingestBuilder) Build() *Ingest {
+	chunkPool := sync.Pool{New: func() interface{} {
+		chunk := chunk
+		return chunk
+	}}
+	textPool := sync.Pool{New: func() interface{} {
+		text := ""
+		return text
+	}}
+	in := &Ingest{
+		Path:      ib.path,
+		ChunkPool: &chunkPool,
+		TextPool:  &textPool,
+	}
+	return in
+}
+
+func New() IngestBuilder {
+	ib := &ingestBuilder{}
+	return ib
+}
